b6ea03ab24604de2166666919c1319f1d323f7b7
TehmoorG <tehmoor.tg@gmail.com>
2023-09-29 20:47:46 +0100

    Create individual README and Use best practices
    Created a README for sudoku project and changed the code so it met best practices

---
+++ sudoku/README.md
@@ -0,0 +1,33 @@
+# Sudoku Solver using Backtracking
+
+This repository contains a Sudoku solver implemented using the backtracking algorithm. It's a part of the larger "python projects" repository.
+
+## Overview
+
+The Sudoku solver is a simple yet efficient implementation that uses the backtracking algorithm to solve any valid Sudoku puzzle. Users can input their Sudoku board row by row, and the solver will attempt to find a solution and display the solved board.
+
+## Features
+
+- **User-friendly Input**: Users can easily input their Sudoku board row by row.
+- **Efficient Algorithm**: Uses backtracking to efficiently solve the puzzle.
+- **Formatted Display**: The solved board is displayed in a well-formatted manner for easy readability.
+
+## Installation and Usage
+
+1. Clone the "python projects" repository.
+2. Navigate to the Sudoku Solver directory.
+3. Run the script using Python:
+
+```bash
+python sudoku_solver.py
+```
+
+1. Follow the on-screen prompts to input your Sudoku board.
+2. View the solved board once the algorithm completes its execution.
+
+## Dependencies
+- Python 3.x
+
+## Future Enhancements
+
+- **Graphical User Interface**: A user-friendly GUI is in the works to allow users to input puzzles, view solutions, and interact with the solver in real-time.
--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,37 +1,47 @@
-import pprint
+"""
+Sudoku Solver using Backtracking.
 
-board = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
-]
+This module provides functions to solve a Sudoku puzzle using the backtracking algorithm.
+"""
 
 def find_empty(board):
+    """
+    Find the first empty cell in the Sudoku board.
+    
+    Args:
+    - board (list of list of int): The Sudoku board, represented as a 2D list.
+    
+    Returns:
+    - tuple of int: Row and column indices of the first empty cell.
+    - None: If there are no empty cells.
+    """
     for i in range(len(board)):
         for j in range(len(board)):
             if board[i][j] == 0:
-                return i,j
+                return i, j
     return None
 
-
-def check(board,row,column,number):
-    # check row and column for same number   
+def check(board, row, column, number):
+    """
+    Check if it's valid to place a number in a given cell.
+    
+    Args:
+    - board (list of list of int): The Sudoku board.
+    - row (int): Row index of the cell.
+    - column (int): Column index of the cell.
+    - number (int): The number to check.
+    
+    Returns:
+    - bool: True if it's valid to place the number, False otherwise.
+    """
+    # Check row and column for the same number
     for i in range(len(board)):
         if board[row][i] == number or board[i][column] == number:
             return False
 
-    # check each 3x3 grid
-    
-    #checking which row + col the "grid" starts with
-    row_start = (row//3) * 3
-    col_start = (column//3) * 3
-
+    # Check the 3x3 grid the cell belongs to
+    row_start = (row // 3) * 3
+    col_start = (column // 3) * 3
     for i in range(3):
         for j in range(3):
             if board[row_start + i][col_start + j] == number:
@@ -39,20 +49,68 @@ def check(board,row,column,number):
     return True
 
 def solve(board):
+    """
+    Solve the Sudoku board using backtracking.
     
-    #make sure board has empty spaces
-    if not find_empty(board):
+    Args:
+    - board (list of list of int): The Sudoku board.
+    
+    Returns:
+    - bool: True if a solution is found, False otherwise.
+    """
+    # Check if the board has empty spaces
+    empty_cell = find_empty(board)
+    if not empty_cell:
         return True
     
-    row, column = find_empty(board)
-    for num in range(1,10):
-        if check(board,row,column,num):
+    row, column = empty_cell
+    for num in range(1, 10):
+        if check(board, row, column, num):
             board[row][column] = num
             if solve(board):
                 return True
             board[row][column] = 0
     return False
-solve(board)
 
-new_board = pprint.PrettyPrinter(width=40, compact=True)
-new_board.pprint(board)
+def print_board(board):
+    """
+    Print the Sudoku board in a formatted manner.
+    
+    Args:
+    - board (list of list of int): The Sudoku board.
+    """
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - -")
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print("| ", end="")
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+def input_board():
+    """
+    Prompt the user to input each row of the Sudoku board.
+    
+    Returns:
+    - list of list of int: The Sudoku board.
+    """
+    board = []
+    print("Enter your Sudoku board. Use '0' for empty cells.")
+    for i in range(9):
+        while True:
+            row = input(f"Enter row {i + 1} (e.g. 5 3 0 0 7 0 0 0 0): ").split()
+            if len(row) == 9 and all(cell.isdigit() and 0 <= int(cell) <= 9 for cell in row):
+                board.append(list(map(int, row)))
+                break
+            else:
+                print("Invalid input. Please enter 9 numbers separated by spaces.")
+    return board
+
+
+if __name__ == "__main__":
+    board = input_board()
+    solve(board)
+    print_board(board)
